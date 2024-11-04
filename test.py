3574c752fa77f438e32110187168632e5c21aa63
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-11-04 20:19:24 +0530

    Adding only basics

---
+++ test.py
@@ -0,0 +1,11 @@
+name=input("Enter your name: ")
+print(name)
+
+age=int(input("Enter your age: "))
+print(age)
+
+height=float(input("Enter your height: "))
+print(height)
+
+exp1=eval(input("type expression: "))
+print(exp1)
