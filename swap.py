5289c002dc076abac3c2d2ed0e9d9939327ab2bb
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-11-05 08:11:13 +0530

    Add

--- swap.py
+++ swap.py
@@ -1,8 +1,15 @@
-x=12
+""" x=12
 y=13
 temp=x
 x=y
 y=temp
+
+print("value of x is: ",x)
+print("value of y is: ",y) """
+
+x=12
+y=13
+x,y=y,x
 
 print("value of x is: ",x)
 print("value of y is: ",y)
