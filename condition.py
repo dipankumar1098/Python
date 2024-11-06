3c31b9506f2346853bf3add2c3e346c87db8c8ee
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-11-06 18:43:57 +0530

    Updated

---
+++ condition.py
@@ -0,0 +1,16 @@
+# If statement
+""" marks = 87
+if marks >= 90:
+ print("You are eligible")
+ 
+print('thank you') """
+
+#if else statement
+mark=93
+if mark>=90:
+     print("You are eligible")
+     
+else:
+     print("you are not eligible")
+     
+     print("thank you")
