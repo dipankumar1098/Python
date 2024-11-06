33dd079fb3924be21b5ef3f8556e3fdeb6657246
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-11-06 18:50:25 +0530

    Updated

--- condition.py
+++ condition.py
@@ -6,11 +6,26 @@ if marks >= 90:
 print('thank you') """
 
 #if else statement
-mark=93
+""" mark=93
 if mark>=90:
      print("You are eligible")
      
 else:
      print("you are not eligible")
      
-     print("thank you")
+     print("thank you") """
+     
+#if elif else statement
+marks=int(input("Enter your mark:"))
+
+if marks>=90:
+    print("Grade o")
+elif marks>=80 and marks<=90:
+    print("grade E")
+elif marks>=70 and marks<=80:
+    print("grade A")
+elif marks>=60 and marks<=700:
+    print("grade B")
+    
+else:
+    print("fail")
